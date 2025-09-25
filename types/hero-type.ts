adef53f3bf38f866208467a91a874ef24aa388a8
touraye <touraye07@gmail.com>
2025-09-25 17:57:12 +0000

    add types - hero

---
+++ types/hero-type.ts
@@ -0,0 +1,8 @@
+export type HeroSlide = {
+    id: number;
+    title: string;
+    description: string;
+    ctaText: string;
+    ctaHref: string;
+    imageSrc: string; // The main visual for the slide
+};
