43006cfd3f4bb975b6a0d2d8abb31d710f58faa9
touraye <touraye07@gmail.com>
2025-09-29 13:55:05 +0000

    add types - testimonial

---
+++ types/testimonial-types.ts
@@ -0,0 +1,10 @@
+export type Testimonial = {
+    id: number;
+    companyName: string;
+    logo: string;
+    quote: string;
+    authorName: string;
+    authorTitle: string;
+    backgroundImage: string;
+    ctaLink: string;
+};
