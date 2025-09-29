0f294c887f65e76094fe53d3986649b2c5d3d2e0
touraye <touraye07@gmail.com>
2025-09-29 13:53:54 +0000

    add data - insight

---
+++ data/insights-data.ts
@@ -0,0 +1,44 @@
+import { InsightPost } from "@/types/insights-type";
+
+export const insightsData: InsightPost[] = [
+    {
+        id: 1,
+        imageSrc: "/assets/images/home/insight-img1.webp", 
+        title: "DLT in the Real World 2025 - Key Findings",
+        date: "18.06.2025",
+        summary: "DLT in the Real World 2025 - Key Findings",
+        href: "/insights/dlt-2025",
+    },
+    {
+        id: 2,
+        imageSrc: "/assets/images/home/insight-img1.webp",
+        title: "Fund tokenization - A practitioner's point of view",
+        date: "16.04.2025",
+        summary: "Fund tokenization - A practitioner's point of view",
+        href: "/insights/fund-tokenization",
+    },
+    {
+        id: 3,
+        imageSrc: "/assets/images/home/insight-img1.webp",
+        title: "Part 2 - Amendment - Overview of Turkiye regulation on IT systems",
+        date: "31.03.2025",
+        summary: "Part 2 - Overview of Turkiye's regulation on IT systems",
+        href: "/insights/turkiye-regulation",
+    },
+    {
+        id: 4,
+        imageSrc: "/assets/images/home/insight-img1.webp",
+        title: "BT sistemlerine ilişkin Türkiye mevzuatlarına genel bakış - Eklemeler",
+        date: "28.03.2025",
+        summary: "Bölüm 2 - BT sistemlerine ilişkin Türkiye mevzuatlarına genel bakış",
+        href: "/insights/bt-sistemleri",
+    },
+    {
+        id: 5,
+        imageSrc: "/assets/images/home/insight-img1.webp",
+        title: "Another Insight Post Title",
+        date: "25.03.2025",
+        summary: "A brief summary of this interesting insight.",
+        href: "/insights/another-post",
+    },
+];
