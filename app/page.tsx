7b39f64bf4b318fe5a796d5e064ae9c4ec5af049
touraye <touraye07@gmail.com>
2025-09-29 16:10:40 +0000

    update: restructure page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/home/hero/hero-section";
 import Insights from "@/components/home/insights";
 import Resources from "@/components/home/resources";
 import ScrollingCards from "@/components/home/scrolling-cards";
-import Testimonials from "@/components/home/testimonials";
 import TestimonialsSec from "@/components/home/testimonials-sec";
 import UnifiedPlatform from "@/components/home/unified-platform";
 
