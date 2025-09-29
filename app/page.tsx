9fa73d50997b421c2181d8a96ab2d939d46b70f7
touraye <touraye07@gmail.com>
2025-09-29 15:59:15 +0000

    update: add resources

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/home/hero/hero-section";
 import Insights from "@/components/home/insights";
-// import Resources from "@/components/home/resources";
+import Resources from "@/components/home/resources";
 import ScrollingCards from "@/components/home/scrolling-cards";
 import Testimonials from "@/components/home/testimonials";
 import TestimonialsSec from "@/components/home/testimonials-sec";
@@ -27,23 +27,8 @@ export default function Home() {
       {/* Insights */}
       <Insights />
 
-      {/* <Resources /> */}
-
-      {/* Section 2 */}
-      <section className="min-h-screen bg-green-100 flex items-center justify-center">
-        <div className="text-center">
-          <h2 className="text-4xl font-bold text-green-800 mb-4">Section 2</h2>
-          <p className="text-green-600">Green section - min-h-screen</p>
-        </div>
-      </section>
-
-      {/* Section 3 */}
-      <section className="min-h-screen bg-purple-100 flex items-center justify-center">
-        <div className="text-center">
-          <h2 className="text-4xl font-bold text-purple-800 mb-4">Section 3</h2>
-          <p className="text-purple-600">Purple section - min-h-screen</p>
-        </div>
-      </section>
+      {/* resources */}
+      <Resources />     
     </main>
   );
 }
